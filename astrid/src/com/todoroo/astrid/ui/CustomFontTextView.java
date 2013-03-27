664d7b97d6477768b38b78d80cc222a8526e1b80
Sam Bosley <sboz88@gmail.com>
2013-03-27 16:43:44 -0700

    Apply new custom font to several text fields

--- astrid/src/com/todoroo/astrid/ui/CustomFontTextView.java
+++ astrid/src/com/todoroo/astrid/ui/CustomFontTextView.java
@@ -12,6 +12,7 @@ import com.todoroo.astrid.helper.TypefaceCache;
 public class CustomFontTextView extends TextView {
 
     private Typeface defaultTypeface = null;
+    private static final String DEFAULT_FONT = "HandmadeTypewriter.ttf"; //$NON-NLS-1$
 
     public CustomFontTextView(Context context) {
         super(context);
@@ -29,6 +30,7 @@ public class CustomFontTextView extends TextView {
 
     private void setCustomFont(Context context, AttributeSet attrs) {
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CustomFontTextView);
+        boolean setCustomFont = false;
         for (int i = 0; i < ta.getIndexCount(); i++) {
             int index = ta.getIndex(i);
             if (index == R.styleable.CustomFontTextView_customFont) {
@@ -36,6 +38,7 @@ public class CustomFontTextView extends TextView {
                 if (customFont != null) {
                     Typeface tf = TypefaceCache.getTypeface(customFont);
                     if (tf != null) {
+                        setCustomFont = true;
                         defaultTypeface = tf;
                         setTypeface(tf);
                     }
@@ -43,6 +46,15 @@ public class CustomFontTextView extends TextView {
                 break;
             }
         }
+
+        if (!setCustomFont) {
+            Typeface defaultTf = TypefaceCache.getTypeface(DEFAULT_FONT);
+            if (defaultTf != null) {
+                defaultTypeface = defaultTf;
+                setTypeface(defaultTf);
+            }
+        }
+
         ta.recycle();
     }
 
